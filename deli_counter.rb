9b7202d0d285f04558b6be540cccc964b8904c1d
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-03 21:16:27 -0600

    commit tree 7dccf37ace1817dc7e77c2322d4443a349b94ba8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = ["logan", "avi", "spencer"]
+
+def line(katz_deli)
+    if  katz_deli.length == 0
+        puts "The line is currently empty."
+    elsif katz_deli.length > 0
+        phrase = "The line is currently:"
+        katz_deli.each_with_index do |names, index|
+        phrase << " #{index + 1}. #{names}"
+       end 
+       puts phrase
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(other_deli)
+    if other_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{other_deli.first}."
+        other_deli.shift
+    end
+end
